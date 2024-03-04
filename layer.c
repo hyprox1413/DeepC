f9b5c177c4f79363f54157d783f13abeb7e3f1e2
hyprox1413 <hyprox1413@gmail.com>
2024-03-04 16:13:41 -0500

    optimized backpropagation with dynamic programming

--- layer.c
+++ layer.c
@@ -7,6 +7,9 @@
 #include <string.h>
 #include <time.h>
 
+#define OUT_OF_MEMORY (-1)
+#define NAN_ERROR (-2)
+
 typedef struct layer {
   int neurons;
 
@@ -18,6 +21,7 @@ typedef struct layer {
   double *weights;
   double *biases;
   double *activations;
+  double *gradients;
 } layer_t;
 
 /*
@@ -32,30 +36,49 @@ void initialize_model(layer_t *first_layer) {
   assert(first_layer);
   assert(!first_layer->last);
 
+  first_layer->activations = malloc(sizeof(double) * first_layer->neurons);
+  if (!first_layer->activations) {
+      fprintf(stderr, "Out of memory.\n");
+      exit(OUT_OF_MEMORY);
+  }
   layer_t *cur_layer = first_layer->next;
   while (cur_layer) {
 
     /* remember to free() these at some point lol */
 
+    cur_layer->biases = malloc(sizeof(double) * cur_layer->neurons);
+    if (!cur_layer->biases) {
+      fprintf(stderr, "Out of memory.\n");
+      exit(OUT_OF_MEMORY);
+    }
+    for (int i = 0; i < cur_layer->neurons; i++) {
+      cur_layer->biases[i] = 0.0;
+    }
+    cur_layer->activations = malloc(sizeof(double) * cur_layer->neurons);
+    if (!cur_layer->activations) {
+      fprintf(stderr, "Out of memory.\n");
+      exit(OUT_OF_MEMORY);
+    }
     cur_layer->weights = malloc(sizeof(double)
         * cur_layer->neurons * cur_layer->last->neurons);
     if (!cur_layer->weights) {
-      fprintf(stderr, "Out of memory.");
+      fprintf(stderr, "Out of memory.\n");
+      exit(OUT_OF_MEMORY);
     }
-    cur_layer->biases = calloc(cur_layer->neurons, sizeof(double));
-    if (!cur_layer->weights) {
-      fprintf(stderr, "Out of memory.");
-    }
-    cur_layer->activations = calloc(cur_layer->neurons, sizeof(double));
-    if (!cur_layer->activations) {
-      fprintf(stderr, "Out of memory.");
-    }
+
+    /* Xavier initialization */
+
     int last_neurons = cur_layer->last->neurons;
     for (int i = 0; i < cur_layer->neurons; i++) {
       for (int j = 0; j < last_neurons; j++) {
         cur_layer->weights[last_neurons * i + j]
           = rand_normal(0, 1.0 / last_neurons);
       }
+    }
+    cur_layer->gradients = malloc(sizeof(double) * cur_layer->neurons);
+    if (!cur_layer->gradients) {
+      fprintf(stderr, "Out of memory.\n");
+      exit(OUT_OF_MEMORY);
     }
     cur_layer = cur_layer->next;
   }
@@ -75,21 +98,23 @@ double *predict(layer_t *first_layer, double *input) {
   assert(!first_layer->last);
   assert(input);
 
-  free(first_layer->activations);
-  first_layer->activations = malloc(sizeof(double) * first_layer->neurons);
   memcpy(first_layer->activations, input,
       sizeof(double) * first_layer->neurons);
   layer_t *cur_layer = first_layer->next;
   while (1) {
-    free(cur_layer->activations);
-    cur_layer->activations = calloc(cur_layer->neurons, sizeof(double));
     for (int i = 0; i < cur_layer->neurons; i++) {
+      cur_layer->activations[i] = 0.0; 
       for (int j = 0; j < cur_layer->last->neurons; j++) {
         cur_layer->activations[i] += cur_layer->last->activations[j]
           * cur_layer->weights[cur_layer->last->neurons * i + j];
       }
       cur_layer->activations[i] += cur_layer->biases[i];
       cur_layer->activations[i] = relu(cur_layer->activations[i]);
+      if (isnan(cur_layer->activations[i])) {
+        fprintf(stderr, "Floating point error.  "
+            "Perhaps the learning rate is too high?\n");
+        exit(NAN_ERROR);
+      }
     }
     if (cur_layer->next) {
       cur_layer = cur_layer->next;
@@ -104,6 +129,8 @@ double *predict(layer_t *first_layer, double *input) {
 /*
  * Calculates the partial derivative of the mean squared error loss
  * with respect to the ith neuron in the passed layer.
+ *
+ * Deprecated since dynamic programming is faster than recursion.
  */
 
 double calc_partial(layer_t *cur_layer, int i, double *output) {
@@ -137,16 +164,36 @@ void train(layer_t *first_layer, double *input, double *output,
 
   predict(first_layer, input);
   layer_t *cur_layer = first_layer->next;
-  while (cur_layer) {
+  while (cur_layer->next) {
+    cur_layer = cur_layer->next;
+  }
+  while (cur_layer->last) {
     for (int i = 0; i < cur_layer->neurons; i++) {
-      double partial = calc_partial(cur_layer, i, output);
-      cur_layer->biases[i] -= learn_rate * partial;
-      for (int j = 0; j < cur_layer->last->neurons; j++) {
-        cur_layer->weights[cur_layer->last->neurons * i + j]
-          -= learn_rate * cur_layer->last->activations[j] * partial; 
+      cur_layer->gradients[i] = 0.0;
+    }
+    if (cur_layer->next) {
+      for (int i = 0; i < cur_layer->neurons; i++) {
+        for (int j = 0; j < cur_layer->next->neurons; j++) {
+          cur_layer->gradients[i]
+            += cur_layer->next->weights[cur_layer->neurons * j + i]
+            * cur_layer->next->gradients[j];
+        }
       }
     }
-    cur_layer = cur_layer->next;
+    else {
+      for (int i = 0; i < cur_layer->neurons; i++) {
+        cur_layer->gradients[i] = -2 * (output[i] - cur_layer->activations[i]);
+      }
+    }
+    for (int i = 0; i < cur_layer->neurons; i++) {
+      cur_layer->biases[i] -= learn_rate * cur_layer->gradients[i];
+      for (int j = 0; j < cur_layer->last->neurons; j++) {
+        cur_layer->weights[cur_layer->last->neurons * i + j]
+          -= learn_rate * cur_layer->last->activations[j]
+          * cur_layer->gradients[i]; 
+      }
+    }
+    cur_layer = cur_layer->last;
   }
 }
 
