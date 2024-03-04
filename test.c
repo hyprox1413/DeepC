c357bb6a174408108bc1534bcfe5dfeb9280c670
hyprox1413 <hyprox1413@gmail.com>
2024-03-04 16:13:19 -0500

    fixed multilayer models

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include "layer.c"
 
-#define INPUT_DIMENSION (1000)
+#define INPUT_DIMENSION (784)
 #define TRAIN_EPOCHS (1000)
 
 #include <stdio.h>
@@ -15,22 +15,27 @@ int main(void) {
   second.neurons = 10;
   first.next = &second;
   second.last = &first;
+  layer_t third = {};
+  third.neurons = 10;
+  second.next = &third;
+  third.last = &second;
   initialize_model(&first);
   double input[INPUT_DIMENSION] = {};
   for (int i = 0; i < INPUT_DIMENSION; i++) {
-    input[i] = 1.0;
+    input[i] = 1;
   }
+  predict(&first, input);
   for (int i = 0; i < 10; i++) {
-    printf("%lf ", second.activations[i]);
+    printf("%lf ", third.activations[i]);
   }
   double output[10] = {};
   for (int i = 0; i < 10; i++) {
     output[i] = i;
   }
   for (int i = 0; i < TRAIN_EPOCHS; i++) {
-    train(&first, input, output, 0.0001);
+    train(&first, input, output, 0.000001);
     for (int j = 0; j < 10; j++) {
-      printf("%lf ", second.activations[j]);
+      printf("%lf ", third.activations[j]);
     }
     printf("\n");
   }
